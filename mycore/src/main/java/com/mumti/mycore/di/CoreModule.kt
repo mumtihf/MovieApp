ff8d9b28e38d2e8a43f4799e04dead43ead63f6f
Mumti Hany Farisa <mumti.hany18@mhs.uinjkt.ac.id>
2021-06-13 00:15:43 +0700

    Add: Certificate Pinning

--- mycore/src/main/java/com/mumti/mycore/di/CoreModule.kt
+++ mycore/src/main/java/com/mumti/mycore/di/CoreModule.kt
@@ -10,6 +10,7 @@ import com.mumti.mycore.domain.repository.IFilmRepository
 import com.mumti.mycore.utils.AppExecutors
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -34,10 +35,17 @@ import java.util.concurrent.TimeUnit
 
     val networkModule = module {
         single {
+            val hostname = "api.themoviedb.org"
+            val certificatePinner = CertificatePinner.Builder()
+                .add(hostname, "sha256/sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+                .add(hostname, "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+                .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+                .build()
             OkHttpClient.Builder()
                 .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
                 .connectTimeout(120, TimeUnit.SECONDS)
                 .readTimeout(120, TimeUnit.SECONDS)
+                .certificatePinner(certificatePinner)
                 .build()
         }
         single {
